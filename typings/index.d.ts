3b30181a54f8989681991f4d4e5ad349c65835f3
IYR3775 <iyaadh360@gmail.com>
2021-05-27 19:32:04 +0500

    Button collector typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,9 +1,12 @@
 declare function _exports(client: Client): {
     MessageButton: typeof MessageButton;
+    ButtonCollector: typeof ButtonCollector;
 };
 declare namespace _exports {
     export { MessageButton };
+    export { ButtonCollector };
 }
 export = _exports;
 import MessageButton = require("./Classes/MessageButton");
+import ButtonCollector = require("./Classes/ButtonCollector");
 import { Client } from 'discord.js';
